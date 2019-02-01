540ffe3fd031b2ab17f54faf3182e95e05fbf433
Carles <https://github.com/CarlesRa>
2019-02-01 09:53:20 +0100

    bucle fins introduir lletres

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,6 +6,6 @@ public class Main {
     public static void main(String[] args) {
         //Exercici01 exe01 = new Exercici01();
         //Exercici02 exe02 = new Exercici02();
-        //Exercici03 exe03 = new Exercici03();
+        Exercici03 exe03 = new Exercici03();
     }
 }
--- src/main/java/exercici03/Exercici03.java
+++ src/main/java/exercici03/Exercici03.java
@@ -1,23 +1,23 @@
 package exercici03;
-import utils.Lib;
 
 import java.util.InputMismatchException;
 import java.util.Scanner;
 public class Exercici03 {
-    private boolean isIntroduced;
-    private int num;
-    private int numMajor;
+    private boolean hayNumeros;
+    private float num;
+    private float numMajor;
     public Exercici03(){
 
-        isIntroduced = true;
+        hayNumeros = true;
         demanarNumeros();
-        if (!isIntroduced){
-            System.out.println("No ha introducido ningun número...");
-        }
-        else{
-            System.out.print("El numero mayor introducido es: " + num);
+        if (hayNumeros) {
+            System.out.print("El numero mayor introducido es: " + numMajor);
         }
     }
+
+    /**
+     * Demana nombres fins que sintrodueixen lletres
+     */
     public void demanarNumeros(){
         Scanner lec = new Scanner(System.in);
         int count = 0;
@@ -25,23 +25,32 @@ public class Exercici03 {
         do{
             System.out.print("Introduce numeros hasta que se introduzcan letras: ");
             try {
-                num = Integer.parseInt(lec.nextLine());
+                num = Float.parseFloat(lec.nextLine());
+                count = 0;
                 if (numMajor<num){
                     count++;
                     numMajor = num;
                 }
             }
-            catch (NumberFormatException NFEe){
-                if (count == 0){
-                    isIntroduced = false;
-                }
-                sonLetras = true;
-            }
             catch (InputMismatchException IMEe){
                 if (count == 0){
-                    this.isIntroduced = false;
+                    hayNumeros = false;
+                    System.out.println("No ha introducido ningun número...");
                 }
-                sonLetras = true;
+                else {
+                    hayNumeros = true;
+                    sonLetras = true;
+                }
+            }
+            catch (NumberFormatException NFEe){
+                if (count == 0){
+                    hayNumeros = false;
+                    System.out.println("No ha introducido ningun número...");
+                }
+                else {
+                    hayNumeros = true;
+                    sonLetras = true;
+                }
             }
         }while (!sonLetras);
     }
