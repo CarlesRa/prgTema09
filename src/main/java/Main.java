ce2bd01468830d89ba9f8f56741b2529d9ac4099
Carles <https://github.com/CarlesRa>
2019-02-01 20:49:02 +0100

    Terminator exercici 04

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,13 @@
 import exercici01.Exercici01;
 import exercici02.Exercici02;
 import exercici03.Exercici03;
+import exercici04.Exercici04;
 
 public class Main {
     public static void main(String[] args) {
         //Exercici01 exe01 = new Exercici01();
         //Exercici02 exe02 = new Exercici02();
-        Exercici03 exe03 = new Exercici03();
+        //Exercici03 exe03 = new Exercici03();
+        Exercici04 exe04 = new Exercici04();
     }
 }
--- src/main/java/exercici04/Exercici04.java
+++ src/main/java/exercici04/Exercici04.java
@@ -1,4 +1,44 @@
 package exercici04;
+import utils.Lib;
 
+import java.util.Arrays;
+import java.util.Scanner;
 public class Exercici04 {
+    private Scanner lec = new Scanner(System.in);
+    private int []vector ;
+    private int []vFail;
+    private boolean esLetra;
+    public Exercici04(){
+        vector = new int[5];
+        vFail = null;
+        rellenarVector(vector);
+        System.out.println(Arrays.toString(vector));
+        rellenarVector(vFail);
+
+    }
+
+    public void rellenarVector(int [] vector){
+        int num = 0;
+        int i = 0;
+        boolean condicion = false;
+        while (!condicion){
+            System.out.print("Introduce un número: ");
+            try {
+                num = Integer.parseInt(lec.nextLine());
+                vector[i] = num;
+                i++;
+            } catch (NumberFormatException NFEe) {
+                System.out.println("No puedes introducir letras.");
+                Lib.continuar();
+            }
+            catch (ArrayIndexOutOfBoundsException AIOOBEe){
+                System.out.println("Ha superado la capacidad de almacenamiento.");
+                condicion = true;
+            }
+            catch (NullPointerException NPEe){
+                System.out.println("Es un vector vacio... no se pueden introducir valores.");
+                condicion = true;
+            }
+        }
+    }
 }
